711e2d7242bdba27cf46ecd0ddfc1a5b49c1d480
Liav Edry <liav.acc@gmail.com>
2023-09-21 01:05:36 +0300

    fix build

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,8 @@ export default function Home() {
         </h1>
         <code className={styles.tagline}>Web Developer &amp;&amp; Next.js || JavaScript</code>
         <div className={styles.iconsSocials}>
-          <a target="_blank" title="GitHub Profile" href="https://github.com/Devail1"><i className="fab fa-github"></i></a>
-          <a target="_blank" title="LinkedIn Profile" href="https://www.linkedin.com/in/liav-edry/"><i className="fab fa-linkedin"></i></a>
+          <a target="_blank" title="GitHub Profile" rel="noreferrer" href="https://github.com/Devail1"><i className="fab fa-github"></i></a>
+          <a target="_blank" title="LinkedIn Profile" rel="noreferrer" href="https://www.linkedin.com/in/liav-edry/"><i className="fab fa-linkedin"></i></a>
         </div>
       </main>
     </div>
