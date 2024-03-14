48db139ec0a5e00eabdf55c939beb35f83b50aab
Liav Edry <liav.acc@gmail.com>
2024-03-14 16:04:29 +0200

    fix lint errors in client

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <button className="btn btn-primary">Hello daisy</button>
-      <a href="/api/hello" className="link">
+      <button type="button" className="btn btn-primary">
+        Hello daisy
+      </button>
+      <Link href="/api/hello" className="link">
         Testing Backend
-      </a>
+      </Link>
     </div>
   );
 }
