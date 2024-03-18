d80412db3f3c3a3cf26beef4d0f09bf98d5053f4
Liav Edry <liav.acc@gmail.com>
2024-03-18 02:17:04 +0200

    added redux persist

--- client/src/components/AppContainer.tsx
+++ client/src/components/AppContainer.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { AppProps } from "next/app";
-import { store } from "@/redux/store";
+import { persistor, store } from "@/redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 import Layout from "./Layout";
 
 function AppContainer({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <PersistGate loading={null} persistor={persistor}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PersistGate>
     </Provider>
   );
 }
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import themeSlice from "@/redux/themeSlice";
 
-export const store = configureStore({
-  reducer: {
-    theme: themeSlice.reducer,
-  },
-  devTools: process.env.NODE_ENV !== "production" ? true : false,
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["theme"],
+};
+
+const rootReducer = combineReducers({
+  theme: themeSlice.reducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== "production",
+});
+
+export const persistor = persistStore(store);
 
 // Infer the type of the store state
 export type RootState = ReturnType<typeof store.getState>;
