25ff9afc097f20016bbe9908ecd75ae7de21fa2c
Liav Edry <liav.acc@gmail.com>
2024-03-19 18:40:22 +0200

    client/fix build error

--- client/src/pages/mdx-page.tsx
+++ client/src/pages/mdx-page.tsx
@@ -1,33 +1,11 @@
-import { ReactNode, useRef, useState } from "react";
-import { MDXEditor } from "@mdxeditor/editor";
+import { ReactNode } from "react";
 import MDXContent from "../../mock/mdx-content.mdx";
 
 export default function MDXPage({ children }: { children: ReactNode }) {
-  const [editing, setEditing] = useState(false);
-  const ref = useRef(null);
-
-  const handleEdit = () => {
-    setEditing(!editing);
-  };
-
-  const handleSave = async () => {
-    setEditing(false);
-  };
-
   return (
     <div className="prose lg:prose-xl prose-img:rounded-xl prose-img:my-0 prose-a:text-blue-400">
-      <div>
-        <button type="button" onClick={handleEdit}>
-          {editing ? "Cancel" : "Edit"}
-        </button>
-        {editing ? <MDXEditor markdown="Hello world" ref={ref} /> : <MDXContent />}
-        {editing && (
-          <button type="button" onClick={handleSave}>
-            Save
-          </button>
-        )}
-        {children}
-      </div>
+      <MDXContent />
+      {children}
     </div>
   );
 }
