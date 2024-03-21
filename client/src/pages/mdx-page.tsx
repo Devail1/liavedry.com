1cd04d15543b53eb105f46901d7c1810f7bce1eb
Liav Edry <liav.acc@gmail.com>
2024-03-21 05:01:34 +0200

    testing ci cd

--- client/src/pages/mdx-page.tsx
+++ client/src/pages/mdx-page.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react";
 import MDXContent from "../../mock/mdx-content.mdx";
+import Head from "next/head";
 
 export default function MDXPage({ children }: { children: ReactNode }) {
   return (
-    <div className="prose lg:prose-xl prose-img:rounded-xl prose-img:my-0 prose-a:text-blue-400">
-      <MDXContent />
-      {children}
-    </div>
+    <>
+      <Head>
+        <title>Liav Edry | Blog</title>
+      </Head>
+      <div className="prose lg:prose-xl prose-img:rounded-xl prose-img:my-0 prose-a:text-blue-400">
+        <MDXContent />
+        {children}
+      </div>
+    </>
   );
 }
